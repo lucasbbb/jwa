1470bfd8d7d2e9cbe416f84a2d075032e813ece7
lucasbbb <lucasbbb@qq.com>
2022-04-01 15:26:08 +0800

    feat: create jwa and signature

---
+++ jwa.go
@@ -0,0 +1,8 @@
+// Copyright 2022 lucasbbb. All rights reserved.
+// Use of this source code is governed by an Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package jwa defines the names of all the algorithms which can be used by
+// JSON Web Signature (JWS), JSON Web Encryption (JWE), and JSON Web Key (JWK).
+// https://www.rfc-editor.org/rfc/rfc7518#section-1
+package jwa
